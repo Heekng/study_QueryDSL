d263ca6694e9a7aed626b83c3e5f355e06e3dd06
heekng <rhkd1769@gmail.com>
2022-05-14 17:05:19 +0900

    QuerydslPredicateExecutor

--- src/test/java/com/heekng/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/com/heekng/querydsl/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package com.heekng.querydsl.repository;
 import com.heekng.querydsl.dto.MemberSearchCondition;
 import com.heekng.querydsl.dto.MemberTeamDto;
 import com.heekng.querydsl.entity.Member;
+import com.heekng.querydsl.entity.QMember;
 import com.heekng.querydsl.entity.Team;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
@@ -98,5 +99,30 @@ class MemberRepositoryTest {
         //then
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+    }
+
+    @Test
+    void querydslPredicateExcutorTest() throws Exception {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+        for (Object o : result) {
+            System.out.println("o = " + o);
+        }
     }
 }
