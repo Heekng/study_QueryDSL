93ebd921a65e50cc89e2fb2673ce296d7add394d
heekng <rhkd1769@gmail.com>
2022-05-14 14:21:06 +0900

    BooleanBuilder 사용

--- src/test/java/com/heekng/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/heekng/querydsl/QuerydslBasicTest.java
@@ -7,6 +7,7 @@ import com.heekng.querydsl.entity.Member;
 import com.heekng.querydsl.entity.QMember;
 import com.heekng.querydsl.entity.QTeam;
 import com.heekng.querydsl.entity.Team;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -599,5 +600,33 @@ public class QuerydslBasicTest {
         for (MemberDto memberDto : result) {
             System.out.println("memberDto = " + memberDto);
         }
+    }
+
+    @Test
+    void dinamicQuery_BooleanBuilder() throws Exception {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+
+        // 필수 초기값 지정이 가능하다.
+//        BooleanBuilder builder = new BooleanBuilder(member.username.eq(usernameCond));
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 }
