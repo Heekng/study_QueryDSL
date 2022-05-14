5f644b13842cd39c2930c3ab0f2acf9ce478b60f
heekng <rhkd1769@gmail.com>
2022-05-14 15:33:59 +0900

    동적 쿼리, 성능 최적화, builder

---
+++ src/main/java/com/heekng/querydsl/dto/MemberTeamDto.java
@@ -0,0 +1,23 @@
+package com.heekng.querydsl.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Data;
+
+@Data
+public class MemberTeamDto {
+
+    private Long memberId;
+    private String username;
+    private int age;
+    private Long teamId;
+    private String teamName;
+
+    @QueryProjection // dto 에 의존한다는 점을 기억할 것.
+    public MemberTeamDto(Long memberId, String username, int age, Long teamId, String teamName) {
+        this.memberId = memberId;
+        this.username = username;
+        this.age = age;
+        this.teamId = teamId;
+        this.teamName = teamName;
+    }
+}
--- src/main/java/com/heekng/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/com/heekng/querydsl/repository/MemberJpaRepository.java
@@ -1,16 +1,24 @@
 package com.heekng.querydsl.repository;
 
+import com.heekng.querydsl.dto.MemberSearchCondition;
+import com.heekng.querydsl.dto.MemberTeamDto;
+import com.heekng.querydsl.dto.QMemberTeamDto;
 import com.heekng.querydsl.entity.Member;
 import com.heekng.querydsl.entity.QMember;
+import com.heekng.querydsl.entity.QTeam;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 import static com.heekng.querydsl.entity.QMember.*;
+import static com.heekng.querydsl.entity.QTeam.*;
+import static org.springframework.util.StringUtils.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -54,6 +62,38 @@ public class MemberJpaRepository {
         return queryFactory
                 .selectFrom(member)
                 .where(member.username.eq(username))
+                .fetch();
+    }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (hasText(condition.getUsername())) {
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+        if (hasText(condition.getTeamName())) {
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+        if (condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+        if (condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return queryFactory
+                .select(
+                        new QMemberTeamDto(
+                                member.id.as("memberId"),
+                                member.username,
+                                member.age,
+                                team.id.as("teamId"),
+                                team.name.as("teamName")
+                        )
+                )
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
                 .fetch();
     }
 }
