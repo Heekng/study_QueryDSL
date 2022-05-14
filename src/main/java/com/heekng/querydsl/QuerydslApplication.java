ec269e206235550358babc347079785d8b56143b
heekng <rhkd1769@gmail.com>
2022-05-14 15:16:26 +0900

    순수 JPA 리포지토리 + querydsl

--- src/main/java/com/heekng/querydsl/QuerydslApplication.java
+++ src/main/java/com/heekng/querydsl/QuerydslApplication.java
@@ -1,7 +1,11 @@
 package com.heekng.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
@@ -10,4 +14,10 @@ public class QuerydslApplication {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
 
+	// bean으로 등록함으로써 queryfactory를 인젝션 받아도 된다.
+	// 동시성문제? JpaQueryFactory는 EntityManager에 의존하고, EntityManager은 트랜젝션 단위로 행동하기 때문에 동시성 문제가 없다.
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+		return new JPAQueryFactory(em);
+	}
 }
